9cd98e69f0ae586e48dcbf9db95ad20eb9807aa2
Jarrett Byrnes <jarrett.byrnes@umb.edu>
2019-07-12 12:00:25 -0400

    updating keen name fixes

--- R/QC_KEEN_name_gsub.R
+++ R/QC_KEEN_name_gsub.R
@@ -9,8 +9,10 @@ name_gsub <- . %>%
   mutate(TRANSECT = trimws(TRANSECT)) %>%
   mutate(TRANSECT = gsub("\\&", "and", TRANSECT)) %>%
   mutate(TRANSECT = gsub("DEVILS_DANCE_FLOOR", "Devil's Dance Floor", TRANSECT)) %>%
+  mutate(TRANSECT = gsub("Devils", "Devil's", TRANSECT)) %>%
   mutate(TRANSECT = gsub("BROAD_COVE", "Broad Cove", TRANSECT)) %>%
   mutate(TRANSECT = gsub("BROADWAY_42ND", "Broadway and 42nd", TRANSECT)) %>%
+  mutate(TRANSECT = gsub("Broadway [0-9][0-9]", "Broadway and 42nd", TRANSECT)) %>%
   mutate(TRANSECT = gsub("NORWEGIAN_COVE", "Norwegian Cove", TRANSECT)) %>%
   mutate(TRANSECT = gsub("LARUS_LEDGE", "Larus Ledge", TRANSECT)) %>%
   mutate(TRANSECT = gsub("MAGIC_8_BALL", "Magic 8 Ball", TRANSECT)) %>%
@@ -41,6 +43,12 @@ name_gsub <- . %>%
   mutate(SITE = gsub("Nubble$", "Nubble Lighthouse", SITE)) %>%
   mutate(SITE = gsub("Canoe$", "Canoe Beach", SITE)) %>%
   mutate(SITE = gsub("Pemaquid, ME", "Pemaquid", SITE)) %>%
+  mutate(SITE = gsub("PEMA", "Pemaquid", SITE)) %>%
+  mutate(SITE = gsub("Pema", "Pemaquid", SITE)) %>%
+  mutate(SITE = gsub("HURR", "Hurricane Island", SITE)) %>%
+  mutate(SITE = gsub("Hurr", "Hurricane Island", SITE)) %>%
+  mutate(SITE = gsub("Pumphouse$", "Pumphouse Beach", SITE)) %>%
+  mutate(SITE = gsub("Fort Wetherill", "Fort Weatherill", SITE)) %>%
   mutate(SITE = str_remove(SITE, ", Nahant, MA")) %>%
   mutate(TRANSECT = gsub("\\&", "and", TRANSECT)) %>%
   mutate(TRANSECT = gsub("DEVILS_DANCE_FLOOR", "Devil's Dance Floor", TRANSECT)) %>%
@@ -53,6 +61,8 @@ name_gsub <- . %>%
   mutate(TRANSECT = gsub("SANDPIPER", "Sandpiper Beach", TRANSECT)) %>%
   mutate(TRANSECT = gsub("NORTH_PEPPERRELL", "North Pepperrell", TRANSECT)) %>%
   mutate(TRANSECT = gsub("NORTH_SMITHS_COVE", "North Smith's Cove", TRANSECT)) %>%
+  mutate(TRANSECT = gsub("North Smith$", "North Smith's Cove", TRANSECT)) %>%
+  mutate(TRANSECT = gsub("South Smith$", "South Smith's Cove", TRANSECT)) %>%
   mutate(TRANSECT = gsub("SOUTH_PEPPERRELL", "South Pepperrell", TRANSECT)) %>%
   mutate(TRANSECT = gsub("SOUTH_SMITHS_COVE", "South Smith's Cove", TRANSECT)) %>%
   mutate(TRANSECT = gsub("Broad cove", "Broad Cove", TRANSECT)) %>%
